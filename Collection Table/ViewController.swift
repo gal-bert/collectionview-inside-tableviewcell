fa93842255a4ed4d04b4398f03d81ee6978e2a63
Gregorius Albert <galbert.djaja@gmail.com>
2022-09-29 23:42:02 +0700

    Initial Commit

---
+++ Collection Table/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Collection Table
+//
+//  Created by Gregorius Albert on 29/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
